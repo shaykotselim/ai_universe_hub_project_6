70dcab204b605b4c3b58c4fdf776b1da688e93b5
shaykot43500 <hossainshaykot@gmail.com>
2023-03-04 18:51:50 +0600

    added details & validation area

--- js/index.js
+++ js/index.js
@@ -80,3 +80,95 @@ const displayTools = (tools) => {
   })
       
 }
+// <--------------Load  Details Area---------->
+const loadDetails = async id => {
+  const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data)
+  displayAIDetail(data.data, data.data.features);
+}
+
+// <---------------Show Details into the Modal------------->
+const displayAIDetail = (detail, features) => {
+  console.log(detail, features)
+  const { feature_name } = features
+
+  const modalBody = document.getElementById('modalBody');
+  modalBody.innerHTML = `
+  <div class=" min-width ">
+  
+      <div class=" gap-5  d-flex">
+          <div class="col bg-danger-subtle d-flex ">
+              <div class="border border-danger-subtle rounded p-3 sm:p-1 ">
+              <h3>${detail.description}</h3>
+              <div class="d-flex  gap-4 mt-5 text-center">
+              <div class="card-body container ">
+                  <div class=" border border-danger-subtle rounded fw-bold py-3 sm:py-1 text-success border-0 bg-light opacity-75 h-100">
+                  <p>${detail.pricing ? detail.pricing[0].price : 'Not Available'}</p>
+                  <p>${detail.pricing ? detail.pricing[0].plan : ''}</p>
+                  </div>
+              </div>
+              <div class="card-body container sm:p-5">
+                  <div style="color:#F28927" class="border border-danger-subtle rounded fw-bold py-3 border-0 bg-light opacity-75 h-100 ">
+                  <p>${detail.pricing ? detail.pricing[1].price : 'Not Available'}</p>
+                  <p>${detail.pricing ? detail.pricing[1].plan : ''}</p>
+                  </div>
+              </div>
+              <div class="card-body container h-100 sm:p-5">
+                  <div class="border border-danger-subtle rounded fw-bold py-3 text-danger border-0 bg-light opacity-75">
+                  <p>${detail.pricing ? detail.pricing[2].price : 'Not Available'}</p>
+                  <p>${detail.pricing ? detail.pricing[2].plan : ''}</p>
+                  </div>
+              </div>
+              </div>
+          <div class="d-flex justify-content-between mx-5 mt-3">
+                  <div>
+                      <h4>Features</h4>
+                      <ul>
+                      ${detail.features['1']? `<li>${detail.features['1'].feature_name}</li>` : ' '}
+                      ${detail.features['2']? `<li>${detail.features['2'].feature_name}</li>` : ' '}
+                      ${detail.features['3']? `<li>${detail.features['3'].feature_name}</li>` : ' '}
+                      ${detail.features['4']? `<li>${detail.features['4'].feature_name}</li>` : ' '}
+                      
+                      </ul>  
+                  </div>
+                  
+                  <div>
+                      <h4>Integrations</h4>
+                      <ul>
+                     
+                      </ul>
+                  </div>
+
+          </div>
+
+      </div>
+
+      </div>
+      <div class="col">
+          <div class="border border-danger-subtle p-3 rounded h-100 ">
+              <div class="card-imf-overlay">
+                  <img class="img-fluid rounded"  src="${detail.image_link[0] ? detail.image_link[0] : detail.image_link[1]}"alt="">
+
+                  <p id="accuracy" class=" d-none m-5  bg-danger rounded-pill text-light badge p-2 position-absolute top-0 end-0 ">${detail.accuracy.score * 100}% Accuracy</p>
+
+              </div>
+              <div class="card-body text-center mt-4">
+                  <h5 class="card-title">${detail.input_output_examples ? detail.input_output_examples[0].input : "No! Not Yet! Take a break!!!"}</h5>
+                  <p class="card-text">${detail.input_output_examples ? detail.input_output_examples[0].output : ""}</p>
+              </div>
+          </div>
+      </div>
+      
+  <div>
+  `;
+  // <-------------Validation for null Accuracy ------------>
+  const accuracy = document.getElementById('accuracy');
+  if (detail.accuracy.score === null) {
+      accuracy.classList.add('d-none')
+  }
+  else {
+      accuracy.classList.remove('d-none')
+  }
+}
